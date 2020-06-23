5e9646d2b81ccc9796e36750c405a2cbc71b9d72
Pedro Frohmut <pedrofrohmut@gmail.com>
2020-06-23 11:40:48 -0300

    ChannelList

---
+++ src/components/ChannelButton/index.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+import { Container, HashtagIcon, InviteIcon, SettingsIcon } from "./styles"
+
+export interface Props {
+  channelName: string
+  selected?: boolean
+}
+
+const ChannelButton: React.FC<Props> = ({ channelName, selected }) => (
+  <Container className={selected ? "ChannelButton active" : "ChannelButton"}>
+    <div>
+      <HashtagIcon />
+      <span>{channelName}</span>
+    </div>
+    <div>
+      <InviteIcon />
+      <SettingsIcon />
+    </div>
+  </Container>
+)
+
+export default ChannelButton
---
+++ src/components/ChannelList/index.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+import ChannelButton from "../ChannelButton"
+
+import { Container, Category, AddCategoryIcon } from "./styles"
+
+const ChannelList: React.FC = () => (
+  <Container>
+    <Category>
+      <span>CANAIS DE TEXTO</span>
+      <AddCategoryIcon />
+    </Category>
+    <ChannelButton channelName="chat-livre" />
+    <ChannelButton channelName="trabalho" />
+    <ChannelButton channelName="lolzinho" />
+    <ChannelButton channelName="csgo" />
+    <ChannelButton channelName="valorant" />
+  </Container>
+)
+
+export default ChannelList
